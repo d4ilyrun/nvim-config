409a3cf1359dd6193f6838f83cfc0cb15edf580d
Léo DUBOIN <leo@duboin.com>
2023-12-19 01:07:21 +0100

    feat(keymaps): deactivate arrow keys

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -12,3 +12,9 @@ remap('n', '<C-M-up>', ':resize +1<cr>', { noremap = true })
 remap('n', '<C-M-down>', ':resize -1<cr>', { noremap = true })
 remap('n', '<C-M-right>', ':vertical resize +3<cr>', { noremap = true })
 remap('n', '<C-M-left>', ':vertical resize -3<cr>', { noremap = true })
+
+-- disable arrow keys
+remap('n', '<Up>', '<Nop>', { noremap = true })
+remap('n', '<Down>', '<Nop>', { noremap = true })
+remap('n', '<Left>', '<Nop>', { noremap = true })
+remap('n', '<Right>', '<Nop>', { noremap = true })
